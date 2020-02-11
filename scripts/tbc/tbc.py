7d6a3109d1bca2fa50b0c30ffb36369ebdf7bb93
Victor Agababov <vagababov@gmail.com>
2020-02-11 14:41:06 -0800

    more changes

--- scripts/tbc/tbc.py
+++ scripts/tbc/tbc.py
@@ -5,9 +5,9 @@
 """
 
 
-from math import ceil
 from absl import app
 from absl import flags
+from math import ceil
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,7 +23,7 @@ flags.DEFINE_integer('tbc', 200, 'The target burst capacity', lower_bound=1)
 
 flags.DEFINE_integer('min_traffic', 0, ' Traffic range lower bound for plotting', lower_bound=0)
 flags.DEFINE_integer('max_traffic', 1000, 'Traffic range uppper bound for plotting', upper_bound=10**8, lower_bound=1)
-flags.DEFINE_integer('step_count', 100, 'Max number of points for plotting', lower_bound=5, upper_bound=200)
+flags.DEFINE_integer('step_count', 100, 'Max number of points for plotting', lower_bound=5, upper_bound=500)
 
 
 
@@ -44,27 +44,36 @@ def main(argv):
   num_pods = (ts/int(ceil((FLAGS.target*FLAGS.target_utilization)))).astype(int)
   total_cap = num_pods*FLAGS.target
   util_cap = total_cap*FLAGS.target_utilization
-  fig, graphs = plt.subplots(3)
+  fig, graphs = plt.subplots(4)
   graphs[0].set_title('Number of Pods vs Traffic')
+  graphs[0].set_ylabel('Num Pods')
   graphs[0].step(ts, num_pods, where='post', label='Num Pods')
 
+  graphs[1].set_ylabel('Requests')
   graphs[1].set_title('Effective vs Total Capacity')
   graphs[1].step(ts, total_cap, where='post', label='Total Capacity')
   graphs[1].step(ts, util_cap, where='post', label='Effective Capacity')
 
 
   avl_cap = total_cap-ts
-  print("Available: ", avl_cap)
   exc_cap = avl_cap-FLAGS.tbc
-  print("Excess   : ", exc_cap)
+  graphs[2].set_ylabel('Requests')
   graphs[2].set_title('Available vs Excess Capacity')
   graphs[2].step(ts, exc_cap, where='post', label='Excess Capacity')
   graphs[2].step(ts, avl_cap, where='post', label='Available Capacity')
 
+  proxy = exc_cap / np.absolute(exc_cap)
+  graphs[3].set_ylabel('Proxy Mode?')
+  graphs[3].step(ts, proxy, 'g^', where='post', label='Proxy?')
+
+
   for g in graphs:
     g.legend()
     g.grid(True)
+    g.set_xlabel('Concurrent Requests')
   plt.show()
 
 if __name__ == '__main__':
   app.run(main)
+
+
